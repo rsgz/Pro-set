a3c65329885297c0089bde6378db9d5df63bf9df
rsgz <2966261184@qq.com>
2025-03-08 14:46:24 +0800

    增加了几个vue项目

---
+++ Vue_pro/1-vue+组合式api(函数式)+setup语法糖+js/1-点击按钮显示数字 自增 vite构建/pro_v1/src/components/ShowString.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+<div id="app">
+  <button @click="count++" :disabled="false"> Count is: {{ count }}</button>
+</div>
+</template>
+
+<style scoped>
+/* 如果有样式需求可以在这里添加 */
+</style>
