4b802267f48e857c7e6c94568124ebf083f84ca1
rsgz <2966261184@qq.com>
2025-03-08 15:13:44 +0800

    增加了项目

---
+++ Vue_pro/1.2-vue+组合式api(函数式)+没有语法糖+js/1-点击按钮实现自增/pro1/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <button @click="increment">
+      Count is: {{ count }}
+    </button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const count = ref(1);
+    const increment = () => {
+      count.value++;
+    };
+    return {
+      count,
+      increment
+    };
+  }
+};
+</script>
+
+
+<style scoped>
+button {
+  padding: 10px;
+  font-size: 16px;
+}
+</style>
