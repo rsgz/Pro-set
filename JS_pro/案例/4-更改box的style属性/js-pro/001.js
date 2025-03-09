dafc3f5e8567d3167c842b70503f61bcfa67ea6a
rsgz <2966261184@qq.com>
2025-03-09 14:48:38 +0800

    增加案例修改style属性

---
+++ JS_pro/案例/4-更改box的style属性/js-pro/001.js
@@ -0,0 +1,9 @@
+const box = document.querySelector(".box")
+box.style.width = "300px"
+box.style.height = "300px"
+box.innerText = "300px"
+box.style.backgroundColor = "orange" // 多组单词 就采用 小驼峰命名法
+box.style.border = "3px solid red"
+box.style.borderTop = "3px solid black"
+box.style.paddingLeft = "200px"
+box.style.marginLeft = "200px"
