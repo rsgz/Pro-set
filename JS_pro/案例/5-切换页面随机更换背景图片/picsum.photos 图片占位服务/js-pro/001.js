5800065ebe81999da9c8c9c094659cb83a797076
rsgz <2966261184@qq.com>
2025-03-09 15:12:07 +0800

    增加了图片占位服务的推荐写了两个

---
+++ JS_pro/案例/5-切换页面随机更换背景图片/picsum.photos 图片占位服务/js-pro/001.js
@@ -0,0 +1,15 @@
+// 获取 start-end的随机数 包含 start end
+function get_rand_int_num(start, end) {
+    return Math.floor(
+        Math.random() * (end - start + 1)
+    ) + start
+}
+
+//  picsum.photos  图片占位服务
+// https://picsum.photos/id/1023/200/300
+// `https://picsum.photos/id/${num}/200/300`
+// document.body.style.backgroundImage = `./images/${num}.webp`  // 本地写法
+let num = get_rand_int_num(1, 1000)
+document.body.style.backgroundImage = `url(https://picsum.photos/id/${num}/200/300)`  // 可以显示
+document.body.style.backgroundImage = `url(https://picsum.photos/id/${num}/800/800)`  // 可以
+document.body.style.backgroundImage = `url(https://picsum.photos/id/${num}/1920/920)`  // 可以 1920*920尺寸是可以的
