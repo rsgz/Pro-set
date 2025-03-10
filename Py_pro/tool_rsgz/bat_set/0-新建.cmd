9d800456df57ef94353816e95ff8a0bb2c381221
rsgz <2966261184@qq.com>
2025-03-10 10:25:54 +0800

    新建cmd脚本里面增加了中文编码

--- Py_pro/tool_rsgz/bat_set/0-新建.cmd
+++ Py_pro/tool_rsgz/bat_set/0-新建.cmd
@@ -1,8 +1,10 @@
 set a=@echo off
+set aa=chcp 65001
 set b=mode con: cols=80 lines=25
 set c=color 09
 set d=pause
 echo %a% > 0-001.bat
+echo %aa% > 0-001.bat
 echo %b% >> 0-001.bat
 echo %c% >> 0-001.bat
 echo %d% >> 0-001.bat
