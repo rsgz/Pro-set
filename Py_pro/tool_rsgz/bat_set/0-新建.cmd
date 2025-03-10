cbd99f7c8b3eacf4db829e6817d0f7329ee87967
rsgz <2966261184@qq.com>
2025-03-10 10:24:56 +0800

    增加了git-bat方式拉取最新代码和推送代码

--- Py_pro/tool_rsgz/bat_set/0-新建.cmd
+++ Py_pro/tool_rsgz/bat_set/0-新建.cmd
@@ -2,7 +2,7 @@ set a=@echo off
 set b=mode con: cols=80 lines=25
 set c=color 09
 set d=pause
-echo %a% > 0-新建bat.bat
-echo %b% >> 0-新建bat.bat
-echo %c% >> 0-新建bat.bat
-echo %d% >> 0-新建bat.bat
+echo %a% > 0-001.bat
+echo %b% >> 0-001.bat
+echo %c% >> 0-001.bat
+echo %d% >> 0-001.bat
---
+++ 拉取最新.bat
@@ -0,0 +1,5 @@
+@echo off 
+mode con: cols=80 lines=25 
+color 09 
+git pull origin main
+pause 
