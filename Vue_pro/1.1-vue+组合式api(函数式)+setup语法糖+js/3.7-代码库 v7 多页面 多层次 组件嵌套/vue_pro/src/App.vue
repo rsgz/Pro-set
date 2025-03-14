7aa72fb4e497042503b2e45b8718bf2f570bbb25
rsgz <2966261184@qq.com>
2025-03-14 17:51:48 +0800

    vue多页面多层次组件嵌套
    vue多页面多层次组件嵌套

---
+++ Vue_pro/1.1-vue+组合式api(函数式)+setup语法糖+js/3.7-代码库 v7 多页面 多层次 组件嵌套/vue_pro/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- 只保留路由视图 -->
+  <router-view></router-view>
+</template>
+
+<script setup>
+// 不需要额外的逻辑
+</script>
+
+<style>
+/* 全局样式 */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  /* color: #848b95; */
+  /* color: #2c3e50; */
+  /* margin-top: 60px; */
+}
+</style>
